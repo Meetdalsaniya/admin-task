73585e22e833d0f3656b5fd635539efdb114870e
meetdalsaniya <dalsaniyameet40@gmail.com>
2025-01-05 14:42:41 +0530

    minor change

--- src/sections/login/login-form.jsx
+++ src/sections/login/login-form.jsx
@@ -6,36 +6,93 @@ import {
   Typography,
   Container,
   Paper,
-  Link,
 } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
-
 import { loginUser } from "../../redux/thunks/authThunk";
 import { useNavigate } from "react-router-dom";
 import { showSuccess } from "../../utils/toastUtil";
+import {
+  validateEmail,
+  validatePassword,
+  validateRequired,
+} from "../../utils/validationUtil";
 
 const LoginForm = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [formData, setFormData] = useState({ email: "", password: "" });
-  const { loading, error, user } = useSelector((state) => state.auth);
-  console.log(user);
-  console.log(error);
+  const [errors, setErrors] = useState({ email: "", password: "" });
+
+  const { loading } = useSelector((state) => state.auth);
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+
+    // Validate the field as the user types
+    validateField(name, value);
+  };
+
+  const validateField = (name, value) => {
+    let error = "";
+
+    switch (name) {
+      case "email":
+        error = !validateRequired(value)
+          ? "This field is required."
+          : !validateEmail(value)
+          ? "Invalid email address."
+          : "";
+        break;
+      case "password":
+        error = !validateRequired(value)
+          ? "This field is required."
+          : !validatePassword(value)
+          ? "Password must be at least 6 characters."
+          : "";
+        break;
+      default:
+        break;
+    }
+
+    setErrors((prev) => ({
+      ...prev,
+      [name]: error,
+    }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // Validate all fields on submit
+    const emailError = !validateRequired(formData.email)
+      ? "This field is required."
+      : !validateEmail(formData.email)
+      ? "Invalid email address."
+      : "";
+
+    const passwordError = !validateRequired(formData.password)
+      ? "This field is required."
+      : !validatePassword(formData.password)
+      ? "Password must be at least 6 characters."
+      : "";
+
+    if (emailError || passwordError) {
+      setErrors({
+        email: emailError,
+        password: passwordError,
+      });
+      return;
+    }
+
     dispatch(loginUser(formData)).then((res) => {
       if (res.meta.requestStatus === "fulfilled") {
         navigate("/");
         showSuccess("User Login successful!");
-        console.log("User Login");
       }
     });
   };
+
   return (
     <Container
       component="main"
@@ -52,6 +109,7 @@ const LoginForm = () => {
           Login to Account
         </Typography>
         <form onSubmit={handleSubmit}>
+          {/* Email Field */}
           <TextField
             margin="normal"
             required
@@ -61,18 +119,23 @@ const LoginForm = () => {
             autoFocus
             value={formData.email}
             onChange={handleChange}
+            error={!!errors.email}
+            helperText={errors.email}
           />
+
+          {/* Forget Password Button */}
           <Box
             sx={{ display: "flex", justifyContent: "end", marginTop: "5px" }}
           >
             <Button
               onClick={() => navigate("/forget")}
-              variant="body2"
               sx={{ padding: "0", color: "#1976d2" }}
             >
               Forget Password?
             </Button>
           </Box>
+
+          {/* Password Field */}
           <TextField
             margin="normal"
             required
@@ -82,12 +145,11 @@ const LoginForm = () => {
             type="password"
             value={formData.password}
             onChange={handleChange}
+            error={!!errors.password}
+            helperText={errors.password}
           />
-          {/* {error && (
-            <Typography color="error" variant="body2">
-              {error}
-            </Typography>
-          )} */}
+
+          {/* Submit Button */}
           <Button
             type="submit"
             fullWidth
@@ -97,8 +159,10 @@ const LoginForm = () => {
           >
             {loading ? "Signing In..." : "Sign In"}
           </Button>
+
+          {/* Register Link */}
           <Box mt={2}>
-            <Button onClick={() => navigate("/register")} variant="body2">
+            <Button onClick={() => navigate("/register")}>
               Don't have an account? Register
             </Button>
           </Box>
